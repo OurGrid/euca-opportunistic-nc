023e05384375abcddb4f46918ddcd0c6b9a9d505
Tarciso Braz <tarcisocomp@gmail.com>
2013-07-25 19:03:08 -0300

    Created tests and code for PowerDown method.

---
+++ test/org/ourgrid/node/TestNcPowerDown.java
@@ -0,0 +1,99 @@
+package org.ourgrid.node;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.node.idleness.TestIdlenessChecker;
+import org.ourgrid.node.model.InstanceRepository;
+import org.ourgrid.node.util.OurVirtUtils;
+import org.ourgrid.node.util.ResourcesInfoGatherer;
+import org.ourgrid.virt.OurVirt;
+
+import edu.ucsb.eucalyptus.NcPowerDown;
+import edu.ucsb.eucalyptus.NcPowerDownResponseType;
+import edu.ucsb.eucalyptus.NcPowerDownType;
+
+public class TestNcPowerDown {
+	private static final String LINUX = "Linux";
+	private NodeFacade facade;
+	private OurVirt ourvirtMock = Mockito.mock(OurVirt.class);
+	private TestIdlenessChecker testIChecker = new TestIdlenessChecker();
+	private InstanceRepository instanceRepository = new InstanceRepository();
+	private Properties properties;
+	private ResourcesInfoGatherer rIGMock = 
+			Mockito.mock(ResourcesInfoGatherer.class);
+	private static final String SUCCESS_STATUS_MSG = "0";
+	private static final String UNSUCCESS_STATUS_MSG = "2";
+	
+	@Before
+	public void init() throws Exception {
+		properties = new Properties();
+		properties.load(new FileInputStream("WebContent/WEB-INF/conf/euca.conf"));
+		facade = new NodeFacade(properties, testIChecker, rIGMock, instanceRepository);
+		OurVirtUtils.setOurVirt(ourvirtMock);
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void testNCNotAvailable() throws Exception {
+		testIChecker.setIdle(false);
+		
+		NcPowerDown powerDownReq = createPowerDownRequest(
+				String.valueOf(TestUtils.getNewUserId()));
+		
+		facade.powerDown(powerDownReq);
+	}
+	
+	@Test
+	public void testNonUnixOS() throws Exception {
+		NcPowerDown powerDownReq = createPowerDownRequest(
+				String.valueOf(TestUtils.getNewUserId()));
+		
+		Mockito.when(rIGMock.getOSType()).thenReturn("");
+		
+		NcPowerDownResponseType response =  facade.powerDown(powerDownReq)
+				.getNcPowerDownResponse();
+		
+		NcPowerDownType requestType = powerDownReq.getNcPowerDown();
+		
+		Assert.assertTrue(response.get_return());
+		Assert.assertEquals(response.getUserId(), requestType.getUserId());
+		Assert.assertEquals(response.getCorrelationId(), 
+				requestType.getCorrelationId());
+		Assert.assertEquals(response.getStatusMessage(), UNSUCCESS_STATUS_MSG);
+	}
+	
+	@Test
+	public void testMainFlow() throws Exception {
+		NcPowerDown powerDownReq = createPowerDownRequest(
+				String.valueOf(TestUtils.getNewUserId()));
+		
+		Mockito.when(rIGMock.getOSType()).thenReturn(LINUX);
+		
+		NcPowerDownResponseType response =  facade.powerDown(powerDownReq)
+				.getNcPowerDownResponse();
+		
+		NcPowerDownType requestType = powerDownReq.getNcPowerDown();
+		
+		Assert.assertTrue(response.get_return());
+		Assert.assertEquals(response.getUserId(), requestType.getUserId());
+		Assert.assertEquals(response.getCorrelationId(), 
+				requestType.getCorrelationId());
+		Assert.assertEquals(response.getStatusMessage(), SUCCESS_STATUS_MSG);
+	}
+	
+	private NcPowerDown createPowerDownRequest(String userId) {
+		NcPowerDownType powerDownType = new NcPowerDownType();
+		powerDownType.setUserId(userId);
+		powerDownType.setCorrelationId(
+				String.valueOf(TestUtils.getNewCorrelationId()));
+
+		NcPowerDown powerDownReq = new NcPowerDown();
+		powerDownReq.setNcPowerDown(powerDownType);
+		
+		return powerDownReq;
+	}
+}
